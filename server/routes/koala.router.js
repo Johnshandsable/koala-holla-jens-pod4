502a88773ec17537b735d04acdb1f8f4fc76934a
Johnshandsable <Jshands@asu.edu>
2021-02-11 14:06:24 -0600

    added get, post, put, delete routes onto the server

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,16 +1,30 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
-const pool = require('../modules/pool');
 
 // DB CONNECTION
-console.log(pool);
+const pool = require('../modules/pool');
 
+// console.log(pool);
+
+// ROUTE WILL BE /koalas FOR ALL METHODS
 // GET
+koalaRouter.get('/', (req, res) => {
+  console.log('SERVER - GET inside /koalas');
+});
 
 // POST
+koalaRouter.post('/', (req, res) => {
+  console.log('SERVER - POST inside /koalas');
+});
 
 // PUT
-
+koalaRouter.put('/', (req, res) => {
+  console.log('SERVER - PUT inside /koalas');
+});
 // DELETE
+koalaRouter.delete('/', (req, res) => {
+  console.log('SERVER - DELETE inside /koalas');
+});
 
 module.exports = koalaRouter;
